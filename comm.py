ffc19400347e991217a62a5e450f2c067fe6ff06
Ying Sun <home9464@gmail.com>
2021-12-08 21:28:36 -0800

    add code

--- comm.py
+++ comm.py
@@ -1,21 +1,26 @@
 """communication via HC12 module
 
 pip install pyserial
+
+# see what ports are open
+python -m serial.tools.list_ports
+
 """
 
 import time
 import serial
 
 HC12_DEVICE = '/dev/ttyAMA0'
-ser = serial.Serial(port=HC12_DEVICE,
-                    baudrate=9600,
-                    parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE,
-                    bytesize=serial.EIGHTBITS,
-                    timeout=1)
+ser = serial.Serial(port=HC12_DEVICE, baudrate=9600, timeout=2, writeTimeout=0)
 
-print(ser)
+print(ser.is_open)
+
 counter = 0
-while 1:
+while True:
+    #ser.write(bytes(b'your_commands'))
+    #ser.flush()
     x = ser.readline()
     print(x)
+
+    #print('OK')
+    #ser.close()
